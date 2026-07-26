f5d4616b819a5996f025d91f54f4eb55e29a428a
Dikaçu <mehmetaliselcuk666666@gmail.com>
2026-07-26 19:55:17 +0300

    Revert "perf: optimize njord task2 depth geometry"
    This reverts commit 16c21da5d7387ba326881ac212b689b849763c4d.

--- njord/config/vision_profile.py
+++ njord/config/vision_profile.py
@@ -32,11 +32,7 @@ DETECTOR_SPECS = {
         "kwargs": {
             "camera_height_m": 0.25,
             "detection_max_range_m": 12.0,
-            "plane_ransac_iterations": 100,
-            "plane_full_ransac_interval": 5,
-            "plane_tracking_min_confidence": 0.35,
-            "depth_downsample_factor": 2,
-            "geometry_hz": 7.5,
+            "plane_ransac_iterations": 350,
         },
     },
     "ar_tag": {
